2937c5e2ef249a4902b0282339db19bf473a7cc7
Wolney Freitas <wolneyhqf@gmail.com>
2019-03-14 15:37:59 -0300

    Aula ORM Web

---
+++ AulaORMWeb/src/utils/GerarTabelas.java
@@ -0,0 +1,13 @@
+package utils;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class GerarTabelas {
+	
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("aulaorm_web");
+		factory.close();
+	}
+
+}
---
+++ AulaORMWeb/src/utils/Programa.java
@@ -0,0 +1,30 @@
+package utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import entidades.Cliente;
+
+public class Programa {
+	
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("aulaorm_web");
+		
+		EntityManager em = factory.createEntityManager();
+		
+		Cliente cliente1 = new Cliente();
+		cliente1.setNome("João");
+		
+		Cliente cliente2 = new Cliente();
+		cliente2.setNome("Ana");
+		
+		em.getTransaction().begin();
+		em.persist(cliente1);
+		em.persist(cliente2);
+		em.getTransaction().commit();
+		em.close();
+		
+	}
+
+}
